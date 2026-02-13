3a53ed6514797f2847462ad6ecb36264cceb4297
BMNKUNA <bmwnkuna@gmail.com>
2026-02-13 04:05:08 +0200

    linking to cms project in cloud

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -6,11 +6,8 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
-    appId: 'abktrnhl24zvrvy3d0k5f0xk',
-    /**
-     * Enable auto-updates for studios.
-     * Learn more at https://www.sanity.io/docs/studio-auto-updates
-     */
-    autoUpdates: true,
+    appId: 'tw25u6efuvr5307kqmmxgs0l',
+    autoUpdates: true
   }
+  //…
 })
